da3bfce6eba46f4506c132115a9c781528425dd5
Rodrigo Rodrigues Sandeski <sandeski47@gmail.com>
2020-03-15 19:00:15 -0300

    ChessException and ChessPosition

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -56,9 +56,7 @@ public class Board {
 	}
 	
 	public boolean thereIsAPiece(Position position) {
-		if(!positionExists(position)) {
-			throw new BoardException("Position not on the board!");
-		}
+
 		return piece(position) != null;
 	}
 	
